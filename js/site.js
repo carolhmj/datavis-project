ee324f0425b5eabb4608ed2d67947ba958acc964
Erick Kelvin <erickkelvin@hotmail.com>
2017-12-05 14:55:59 -0300

    improved site navbar

--- js/site.js
+++ js/site.js
@@ -3,6 +3,14 @@
 $(document).ready(function() {
     $(".nav-link").click(function(e) {
         e.preventDefault();
-        $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - 70}, 300);
+        if ($(this).hasClass("reset")) {
+            dc.filterAll();
+            dc.redrawAll();
+        }
+        else {
+            $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - 70}, 300);
+            $(".nav-item").removeClass("active");
+            $(this).parent().addClass("active");
+        }
     });
 });
